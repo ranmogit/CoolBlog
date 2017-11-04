28abef3b6a9027ac5bac2733f90183210c0b4e31
luxiaotao1123 <t1341870251@163.com>
2017-11-04 21:02:29 +0800

    2017-11-4

---
+++ src/main/java/cn/lxt/test/GeneratorTest.java
@@ -0,0 +1,26 @@
+package cn.lxt.test;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by apple on 2017/10/7.
+ */
+public class GeneratorTest {
+    public static void main(String[] args)throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("/Users/apple/Documents/Spring-Boot/src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
