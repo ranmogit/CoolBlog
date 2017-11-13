33a47aaae3dff1602eb86d534a073aeedaae3eea
gpslw <gpslw@qq.com>
2017-11-13 16:22:27 +0800

    11-13

---
+++ src/main/java/cn/blog/Swagger2.java
@@ -0,0 +1,40 @@
+package cn.blog;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration  //spring自动加载
+@EnableSwagger2     //启用Swagger2
+public class Swagger2 {
+    /*
+    createRestApi创建docket的bean之后apiInfo()用来创建该Api的基本信息（显示在文档中）
+    select()函数返回一个ApiSelectorBuilder实例用来控制哪些接口暴露给swagger来展现
+    Swagger会扫描该包下所有Controller定义的API，并产生文档内容（除了被@ApiIgnore指定的请求）
+     */
+
+    private Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("cn.blog.controller"))      //扫描Controller所在的包
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact("CoolBlog")
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/cn/blog/controller/LoginController.java
+++ src/main/java/cn/blog/controller/LoginController.java
@@ -2,13 +2,13 @@ package cn.blog.controller;
 
 import cn.blog.bean.User;
 import cn.blog.service.UserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Objects;
 
 /**
  * Created by liuwen on 2017/11/13.
@@ -25,6 +25,11 @@ public class LoginController {
      * @param password 密码
      * @return statu 成功返回200，失败返回500
      */
+    @ApiOperation(value = "登录验证",notes = "根据用户名和密码进行验证")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name",value = "账号名",required = true,dataType = "String"),
+            @ApiImplicitParam(name = "password",value = "密码",required = true,dataType = "String")
+    })
     @RequestMapping(value = "/ajaxLogin")
     public int ajaxLogin(@Param("name")String name,@Param("password")String password){
         User user = new User(name,password);
