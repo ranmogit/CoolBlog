27b66558c31eebcd30c3d0048e1ba9dc50a34dec
PC-201705081430\Administrator <ranmo1212@qq.com>
2017-11-14 14:23:48 +0800

    front

---
+++ src/main/resources/coolblog-admin/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="login-container">
+    <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
+      class="card-box login-form">
+      <h3 class="title">vue-element-admin</h3>
+      <el-form-item prop="username">
+        <span class="svg-container svg-container_login">
+          <icon-svg icon-class="yonghuming" />
+        </span>
+        <el-input name="username" type="text" v-model="loginForm.username" autoComplete="on" placeholder="username" />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container">
+          <icon-svg icon-class="mima"></icon-svg>
+        </span>
+        <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on"
+          placeholder="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+          Sign in
+        </el-button>
+      </el-form-item>
+      <div class='tips'>
+        <span style="margin-right:20px;">username: admin</span>
+        </span> password: admin</span>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "login",
+  data() {
+    const validateUsername = (rule, value, callback) => {
+      if (!isvalidUsername(value)) {
+        callback(new Error("请输入正确的用户名"));
+      } else {
+        callback();
+      }
+    };
+    const validatePass = (rule, value, callback) => {
+      if (value.length < 5) {
+        callback(new Error("密码不能小于5位"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      loginForm: {
+        username: "admin",
+        password: "admin"
+      },
+      loginRules: {
+        username: [
+          { required: true, trigger: "blur", validator: validateUsername }
+        ],
+        password: [{ required: true, trigger: "blur", validator: validatePass }]
+      },
+      loading: false
+    };
+  },
+  methods: {
+    handleLogin() {
+    console.log('!')
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
